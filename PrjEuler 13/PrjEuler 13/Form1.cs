620e86f7edd61eaba48026b23d7bddf36e66b796
unknown <aaronnw2@gmail.com>
2013-11-03 22:02:48 -0500

    main function troubleshooting, still needs some work to get final answer

--- PrjEuler 13/PrjEuler 13/Form1.cs
+++ PrjEuler 13/PrjEuler 13/Form1.cs
@@ -19,14 +19,32 @@ namespace PrjEuler_13
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             string textInput = txtInput.Text;
-            long[][] input = new long[100][];
+            int[][] input = new int[100][];
             for (int i = 0; i < 100; i++)
             {
+                input[i] = new int[50];
                 for (int j = 0; j < 50; j++)
                 {
-                    input[i][j] = Convert.ToInt64(textInput.Substring(i*50,1));
+                    string singleInput = textInput.Substring(i * 50 + 2 * i+ j, 1);
+                    if(singleInput != "\n" || singleInput != "\r")
+                        input[i][j] = Convert.ToInt32(singleInput);
                 }
             }
+            int runningTotal = 0, carryAmount = 0, lastDigit = 0;
+            int[] answer = new int[200];
+            for (int i = 0; i < 50; i++)
+            {
+                runningTotal = carryAmount;
+                carryAmount = 0;
+                for (int j = 0; j < 100; j++)
+                {
+                    runningTotal += input[100 - j - 1][50 - i - 1];
+                }
+                lastDigit = runningTotal % 10;
+                carryAmount = runningTotal - lastDigit / 10;
+                answer[200 - i - 1] = lastDigit;
+            }
+            answer[0] = carryAmount;
         }
     }
 }
