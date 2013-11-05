54c9d54ab1c8c7db76cfced0c36eec57e67de166
unknown <aaronnw2@gmail.com>
2013-11-05 17:08:10 -0500

    problem 16 added

---
+++ PrjEuler16/PrjEuler16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace PrjEuler16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input the number of powers to raise 2, then find the sum of the digits");
+            int power = Convert.ToInt32(Console.ReadLine());
+            BigInteger total = 1;
+            //find the answer of 2^n
+            for (int i = 0; i < power; i++)
+            {
+                total = total * 2;
+            }
+            string digitString = total.ToString();
+            int sumOfDigits = 0;
+            //add all the digits up;
+            for (int i = 0; i < digitString.Length; i++)
+            {
+                sumOfDigits += Convert.ToInt32(digitString.Substring(i,1));
+            }
+            Console.WriteLine("The sum of the digits of 2^{0} is {1}", power, sumOfDigits);
+        }
+    }
+}
