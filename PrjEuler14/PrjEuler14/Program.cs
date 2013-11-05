8e94c2aa563c287506c82ca863b128c79e66510b
unknown <aaronnw2@gmail.com>
2013-11-05 12:19:17 -0500

    Added caching, some speed ups

--- PrjEuler14/PrjEuler14/Program.cs
+++ PrjEuler14/PrjEuler14/Program.cs
@@ -11,33 +11,57 @@ namespace PrjEuler14
         {
             //caching, make a bool array that is false for all values below a million that we hit along the way, as we know that the length will be shorter for those numbers than the one we started from, and therefore cant be the longest sequence
             int mostIterations = 0, valueWithMostIterations = 0;
+            alreadyVisitedNumber[] visitedNumbers = new alreadyVisitedNumber[1000000];
+            for (int i = 0; i < 1000000; i++)
+            {
+                visitedNumbers[i] = new alreadyVisitedNumber();
+            }
             for (int i = 2; i < 1000000; i++)
             {
-                int numberOfIterations = 0;
-                long testNumber = i;
-                while (testNumber != 1)
+                if(visitedNumbers[i].visited != true)
                 {
-                    if ((testNumber % 2) == 0)
+                    long testNumber = i;
+                    int numberOfIterations = 0;
+                    while(testNumber != 1)
                     {
-                        //even
-                        testNumber = testNumber / 2;
+                        if (testNumber % 2 == 0)
+                            testNumber = testNumber / 2;
+                        else
+                            testNumber = (testNumber * 3) + 1;
+                        if(testNumber < 1000000)
+                            if (visitedNumbers[testNumber].visited == true)
+                            {
+                                visitedNumbers[i].visited = true;
+                                visitedNumbers[i].iterations = (numberOfIterations + visitedNumbers[testNumber].iterations);
+                                break;
+                            }
+                        numberOfIterations++;
+
                     }
-                    else
+                    if (visitedNumbers[i].visited != true)
                     {
-                        //odd
-                        testNumber = (3 * testNumber) + 1;
+                        visitedNumbers[i].visited = true;
+                        visitedNumbers[i].iterations = numberOfIterations;
                     }
-                    numberOfIterations++;
                 }
-                //Console.WriteLine(i + ": " + numberOfIterations);
-                //check if its the longest
-                if(numberOfIterations > mostIterations)
+                if (visitedNumbers[i].iterations > mostIterations)
                 {
-                    mostIterations = numberOfIterations;
+                    mostIterations = visitedNumbers[i].iterations;
                     valueWithMostIterations = i;
                 }
             }
             Console.WriteLine("Number with most iterations: {0}, Number of iterations: {1}", valueWithMostIterations, mostIterations);
+        }
+
+        public class alreadyVisitedNumber
+        {
+            public bool visited;
+            public int iterations;
+            public alreadyVisitedNumber()
+            {
+                visited = false;
+                iterations = 0;
+            }
         }
     }
 }
