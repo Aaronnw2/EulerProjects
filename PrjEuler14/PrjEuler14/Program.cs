58b347a930884fd771ec56efc60fcea80677d0e7
unknown <aaronnw2@gmail.com>
2013-11-05 11:16:01 -0500

    Simple solution for problem 14 done, will try a caching solution for a speed up after this

---
+++ PrjEuler14/PrjEuler14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrjEuler14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //caching, make a bool array that is false for all values below a million that we hit along the way, as we know that the length will be shorter for those numbers than the one we started from, and therefore cant be the longest sequence
+            int mostIterations = 0, valueWithMostIterations = 0;
+            for (int i = 2; i < 1000000; i++)
+            {
+                int numberOfIterations = 0;
+                long testNumber = i;
+                while (testNumber != 1)
+                {
+                    if ((testNumber % 2) == 0)
+                    {
+                        //even
+                        testNumber = testNumber / 2;
+                    }
+                    else
+                    {
+                        //odd
+                        testNumber = (3 * testNumber) + 1;
+                    }
+                    numberOfIterations++;
+                }
+                //Console.WriteLine(i + ": " + numberOfIterations);
+                //check if its the longest
+                if(numberOfIterations > mostIterations)
+                {
+                    mostIterations = numberOfIterations;
+                    valueWithMostIterations = i;
+                }
+            }
+            Console.WriteLine("Number with most iterations: {0}, Number of iterations: {1}", valueWithMostIterations, mostIterations);
+        }
+    }
+}
