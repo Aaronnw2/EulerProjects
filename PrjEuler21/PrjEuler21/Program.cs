be664fd234b60041588d7a789036856952b65bf6
unknown <aaronnw2@gmail.com>
2013-11-14 13:06:12 -0500

    Problem 21, brute force solution finished. Runs fairly fast, but more speedups would be possible by setting some better bounds

--- PrjEuler21/PrjEuler21/Program.cs
+++ PrjEuler21/PrjEuler21/Program.cs
@@ -9,6 +9,35 @@ namespace PrjEuler21
     {
         static void Main(string[] args)
         {
+            int[] divisorSumArray = new int[10001];
+            bool[] isAmicable = new bool[10000];
+            //find the sum of divisors for all numbers under 10000
+            for(int i = 1; i <= 10000; i++)
+            {
+                int sumOfDivisors = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sumOfDivisors += j;
+                }
+                divisorSumArray[i] = sumOfDivisors;
+            }
+            int sumOfAmicableNumbers = 0;
+            //find all the amicable numbers
+            for (int i = 1; i < 10000; i++)
+            {
+                for (int j = 1; j < 10000; j++)
+                    if (divisorSumArray[i] == j && divisorSumArray[j] == i && i != j)
+                    {
+                        isAmicable[i] = true;
+                        break;
+                    }
+            }
+            //add all the amicable numbers
+            for (int i = 1; i < 10000; i++)
+                if (isAmicable[i] == true)
+                    sumOfAmicableNumbers += i;
+            Console.WriteLine("Sum of amicable numbers: " + sumOfAmicableNumbers);
         }
     }
 }
